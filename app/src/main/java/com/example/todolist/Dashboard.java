8b718e7aa591707a046159342f3bf7a8ee243926
jonathanrimin2 <riminj@uni.coventry.ac.uk>
2021-02-21 12:17:11 +0800

    Adding Timer Functionality to Dashboard

--- app/src/main/java/com/example/todolist/Dashboard.java
+++ app/src/main/java/com/example/todolist/Dashboard.java
@@ -7,10 +7,13 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -19,11 +22,27 @@ public class Dashboard extends AppCompatActivity {
     CalendarView calendarView;
     TextView dateText;
 
+    //Retrieved From https://www.youtube.com/watch?v=MDuGwI6P-X8
+
+    private static final long START_TIME_IN_MILLIS = 60000;
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+
+    private CountDownTimer mCountDownTimer;
+
+    private boolean mTimerRunning;
+
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendar);
+        setContentView(R.layout.activity_calendar_and_timer);
 
         calendarView = findViewById(R.id.calendarView);
         dateText =findViewById(R.id.dateText);
@@ -36,10 +55,81 @@ public class Dashboard extends AppCompatActivity {
             }
         });
 
+        mTextViewCountDown = findViewById(R.id.countdown);
+
+        mButtonStartPause = findViewById(R.id.button_start_pause);
+        mButtonReset = findViewById(R.id.button_reset);
+
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mTimerRunning){
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+
+        updateCountDownText();
+
 
         //Assign variable
         drawerLayout = findViewById(R.id.drawer_layout);
 
+    }
+
+    private void startTimer(){
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000 ) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                mButtonStartPause.setText("Start");
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+                mButtonReset.setVisibility(View.VISIBLE);
+
+            }
+        }.start();
+
+        mTimerRunning = true;
+        mButtonStartPause.setText("pause");
+        mButtonReset.setVisibility((View.INVISIBLE));
+    }
+
+    private void pauseTimer(){
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mButtonStartPause.setText("Start");
+        mButtonReset.setVisibility(View.VISIBLE);
+    }
+
+    private void resetTimer(){
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartPause.setVisibility(View.VISIBLE);
+    }
+
+    private void updateCountDownText(){
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        mTextViewCountDown.setText(timeLeftFormatted);
     }
 
     public void ClickMenu(View view){
@@ -57,7 +147,7 @@ public class Dashboard extends AppCompatActivity {
 
     public void ClickDashboard(View view){
         //Recreate activity
-        recreate();;
+        recreate();
     }
 
     public void ClickAboutUs(View view){
@@ -70,11 +160,10 @@ public class Dashboard extends AppCompatActivity {
         MainActivity.logout(this);
     }
 
+
     @Override
     protected void onPause() {
         super.onPause();
         MainActivity.closeDrawer(drawerLayout);
-
-
     }
 }
